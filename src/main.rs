bc0836296e08172de49033ac043be4f96a7d0775
John Lewis <github@jlewis.sh>
2025-07-07 04:20:46 -0500

    refactor: trim out old comments

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,6 @@ async fn main() -> Result<()> {
   let config = Config::from_args_and_env(&args)
     .context("failed to build configuration")?;
 
-  // let output = config
-  //   .command()
-  //   .output()
-  //   .into_diagnostic()
-  //   .context("failed to execute `jj log`")?;
-
-  // println!("got output: {len} bytes", len = output.stdout.len());
-
-  // let output = output
-  //   .stdout
-  //   .utf8_chunks()
-  //   .map(|c| c.valid())
-  //   .filter(|c| !c.is_empty())
-  //   .collect::<Vec<_>>()
-  //   .join("");
-
-  // println!("{output}");
-
   let app = App::new(config);
 
   let terminal = ratatui::init();
